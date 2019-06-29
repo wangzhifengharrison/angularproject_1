6894e861eb0000eaf80d43b0b8961f83f74f31c1
zhifeng wang <1124236437@qq.com>
2019-06-29 23:53:46 +1000

    6-11 end

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Product, Comment, ProductService} from '../shared/product.service';
 
@@ -10,6 +10,9 @@ import {Product, Comment, ProductService} from '../shared/product.service';
 export class ProductDetailComponent implements OnInit {
   product: Product;
   comments: Comment[];
+  newRating:number =5;
+  newComment: string = "";
+  isCommentHidder = true;
 
   constructor(private routeInfo: ActivatedRoute,
               private productService: ProductService
@@ -19,6 +22,16 @@ export class ProductDetailComponent implements OnInit {
     let productId: number = this.routeInfo.snapshot.params["productId"];
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment(){
+    let comment = new Comment(0, this.product.id, new Date().toString(), 'someonw', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+    let sum = this.comments.reduce((sum, coment) => sum + coment.rating, 0);
+    this.product.rating = sum/this.comments.length;
+    this.newComment = null;
+    this.newRating=5;
+    this.isCommentHidder = true;
   }
 
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,20 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
   /*装饰：StartsComponent这个组件的rating属性应该由父组件来传递*/
   @Input()
   /*输入属性，从而能够知道后台评价是什么,接收产品组件传给它的星级评价的数值*/
   private rating = 0;
+
+  @Output()
+  private ratingChange: EventEmitter<number> = new EventEmitter();
+
   // rating来自products
   private stars: boolean[];
+  @Input()
+  private readonly: boolean = true;
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  clickStar(index: number) {
+    if(!this.readonly) {
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
